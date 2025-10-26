6b41b4682564f7763417578167567447e52f462b
Mohammad Nacher Rahman Akter <nacheruddin22@gmail.com>
2025-10-26 17:10:12 +0100

    Ejercicio1

---
+++ Tarea 1.1/ej1.js
@@ -0,0 +1,17 @@
+/*
+Lee desde entrada dos variables numéricas A y B.Con ellas se
+pide realizar un algoritmo que intercambie los valores de ambas
+variables y muestre cuánto valen al final las dos variables.
+*/
+
+let prompt = require('prompt-sync')();
+
+let a = prompt("Escribe el numero a: ");
+
+let b = prompt("Escribe el numero b: ");
+
+let bT = b;
+b = a;
+a = bT;
+
+console.log(`a=${a} y b=${b}`);
