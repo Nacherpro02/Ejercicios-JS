2dac3756c21a397d6613b14155cbabc5d7ad8b6b
Mohammad Nacher Rahman Akter <nacheruddin22@gmail.com>
2025-10-26 23:54:16 +0100

    Ejercicio5

---
+++ Tarea 1.1/ej5.js
@@ -0,0 +1,18 @@
+/*
+Algoritmo que lea un número por teclado. En caso de que ese número sea 0 o menor que 0, 
+se saldrá del programa imprimiendo antes un mensaje de error. 
+Si es mayor que 0, se deberá calcular su cuadrado y la raíz cuadrada del mismo, 
+visualizando el número que ha tecleado el usuario y su resultado («Del número X, 
+su potencia es X y su raíz X» ).Para calcular la raíz cuadrada se puede usar 
+la función interna RAIZ(X)  o con una potencia de 0,5.
+*/
+
+let prompt= require("prompt-sync")();
+
+let x = Number(prompt("Introduzca un numero: "));
+
+if (x<=0) {
+    console.log("Error: Numero no puede ser 0 ni negativo")
+} else {
+    console.log("Del número",x,"su potencia es",x*x, "y su raíz",Math.sqrt(x))
+}
